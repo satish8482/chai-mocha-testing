3be4654aa1e9d30ada27f29b94c0b49ce6c05d5f
satish <satish.8@gtcs.com>
2017-10-20 10:52:44 +0530

    mocha chai basic sample

---
+++ test/test.js
@@ -0,0 +1,11 @@
+
+var expect = require('chai').expect;
+
+describe('Basic test', function (){
+
+  it ('should return when true is passed' , function(){
+
+  	 expect(true).to.be.true;
+  });
+
+});
