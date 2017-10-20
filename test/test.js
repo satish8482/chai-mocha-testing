b9b6a3bec3ed0f82885058bd8b65a01a9312d605
satish <satish.8@gtcs.com>
2017-10-20 12:56:19 +0530

    export model file

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('Basic test', function (){
 
-  it ('should return when true is passed' , function(){
+  it.skip ('should return when true is passed' , function(){
 
   	 expect(true).to.be.true;
   });
