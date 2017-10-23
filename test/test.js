e9748751cd48462cb604f18bce3c55574f3c795c
satish <satish.8@gtcs.com>
2017-10-23 18:57:00 +0530

    tried diff example with mocha and chai

--- test/test.js
+++ test/test.js
@@ -1,11 +1,50 @@
 
 var expect = require('chai').expect;
+var should= require('chai').should();
 
 describe('Basic test', function (){
 
-  it.skip ('should return when true is passed' , function(){
+  it('should return when true is passed' , function(){
 
   	 expect(true).to.be.true;
   });
 
+});
+
+
+
+
+// testing functions
+
+describe('Math', function() {  
+    describe('#abs()', function() {
+        it('should return positive value of given negative number', function() {
+            expect(Math.abs(-5)).to.be.equal(5);
+        });
+    });
+});
+
+
+
+//testing obj and its properties
+describe('object testing' , function(){
+
+  it('should deal with objects' , function(){
+
+   var obj={name :'Jon' , gender:'male'};
+   var objB=obj;
+   obj.should.equal(objB);
+   obj.should.have.property('name').equal('Jon');
+
+
+
+  });
+
+  //testing null objects
+  it ('should allow testing nulls' , function(){
+
+    var iAmNull=null;
+      should.not.exist(iAmNull);
+
+  });
 });
